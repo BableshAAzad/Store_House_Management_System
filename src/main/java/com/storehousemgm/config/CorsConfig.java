18c18cb6524239072eb4b37dc805e7ce63ff7625
bableshaazad <bableshaazad@outllok.com>
2024-08-24 22:29:53 +0530

    cors update 1.7

--- src/main/java/com/storehousemgm/config/CorsConfig.java
+++ src/main/java/com/storehousemgm/config/CorsConfig.java
@@ -3,9 +3,11 @@ package com.storehousemgm.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 //public class CorsConfig{
 public class CorsConfig implements WebMvcConfigurer {
 
@@ -19,6 +21,7 @@ public class CorsConfig implements WebMvcConfigurer {
 //                .allowedOriginPatterns("*")
                 .allowCredentials(true)
                 .allowedHeaders("*")
+                .exposedHeaders("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
 //                .maxAge(3600);
     }
